3dbe9e755d492175c7c44ceff36129d42f6ddfc7
abhayrpatel10 <abhayrpatel10@gmail.com>
2019-06-17 23:08:26 +0800

    file name changes

--- playground.js
+++ playground.js
@@ -18,19 +18,20 @@ app.get('/', (req, res) => {
 
 app.post('/upload',upload.single('myFile') ,(req, res,next) => {
    
-    console.log(req.file)
-    console.log(__dirname)
+    //console.log(req.file)
+    //console.log(__dirname)
+    console.log(req.file.originalname)
     fs.readFile('uploads\\'+req.file.filename,'utf-8',function(err,data){
         if(err){
             return res.status(500).send(err)
         }
         const obfusctaionResult=JavaScriptObfuscator.obfuscate(data)
         const uglyCode=obfusctaionResult.getObfuscatedCode()
-        fs.writeFile('uglycode.js',uglyCode,function(err){
+        fs.writeFile(req.file.originalname,uglyCode,function(err){
             if(err){
                 return res.send(err)
             }
-            res.download(path.join(__dirname,'uglycode.js'))
+            res.download(path.join(__dirname,req.file.originalname))
         })
 
     })
