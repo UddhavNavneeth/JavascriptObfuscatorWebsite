4d661971134e2b87bf16bc4f133195744c865439
abhayrpatel10 <abhayrpatel10@gmail.com>
2019-06-17 17:58:58 +0800

    complete app

---
+++ playground.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const JavaScriptObfuscator = require('javascript-obfuscator');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+let app = express();
+let port = process.env.PORT||3000
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs');
+})
+
+app.post('/upload',upload.single('myFile') ,(req, res,next) => {
+   
+    console.log(req.file)
+    console.log(__dirname)
+    fs.readFile('uploads\\'+req.file.filename,'utf-8',function(err,data){
+        if(err){
+            return res.status(500).send(err)
+        }
+        const obfusctaionResult=JavaScriptObfuscator.obfuscate(data)
+        const uglyCode=obfusctaionResult.getObfuscatedCode()
+        fs.writeFile('uglycode.js',uglyCode,function(err){
+            if(err){
+                return res.send(err)
+            }
+            res.download(path.join(__dirname,'uglycode.js'))
+        })
+
+    })
+    
+   
+})
+
+app.listen(port);
+console.log(`Server is up on port ${port}`);
